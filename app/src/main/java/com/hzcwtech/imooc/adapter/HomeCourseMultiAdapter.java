77fcbee17bbc785215d95bb6e864afae8ac9da5e
talon <lily@hzcwtech.com>
2017-06-13 09:34:19 +0800

    。。。

--- app/src/main/java/com/hzcwtech/imooc/adapter/HomeCourseMultiAdapter.java
+++ app/src/main/java/com/hzcwtech/imooc/adapter/HomeCourseMultiAdapter.java
@@ -1,13 +1,25 @@
 package com.hzcwtech.imooc.adapter;
 
+import android.graphics.Bitmap;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.chad.library.adapter.base.util.MultiTypeDelegate;
 import com.hzcwtech.imooc.R;
 import com.hzcwtech.imooc.entity.model.CourseModel;
 import com.hzcwtech.imooc.entity.model.SkillsModel;
+import com.hzcwtech.imooc.utils.GlideCircleTransform;
+import com.hzcwtech.imooc.utils.GlideRoundTransform;
 
 import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 /**
  * Created by Talon on 2017/6/12.
@@ -51,6 +63,19 @@ public class HomeCourseMultiAdapter extends BaseQuickAdapter<CourseModel, BaseVi
                         .setText(R.id.course_name, course.getName())
                         .setText(R.id.course_short_description, course.getShort_description())
                         .setText(R.id.course_study_numbers, course.getNumbers());
+                final ImageView imageView = helper.getView(R.id.course_name_bg);
+//                Glide.with(mContext).load(course.getPic()).into(new BitmapImageViewTarget(imageView) {
+//                    @Override
+//                    protected void setResource(Bitmap resource) {
+//                        RoundedBitmapDrawable circularBitmapDrawable =
+//                                RoundedBitmapDrawableFactory.create(mContext.getResources(), resource);
+//                        circularBitmapDrawable.setCircular(true);
+//                        imageView.setImageDrawable(circularBitmapDrawable);
+//                    }
+//                });
+//                Glide.with(mContext).load(course.getPic()).apply(new RequestOptions().transform(new RoundedCornersTransformation(mContext, 5,0,RoundedCornersTransformation.CornerType.TOP))).into(imageView);
+
+                Glide.with(mContext).load(course.getPic()).apply(new RequestOptions().transform(new RoundedCornersTransformation(mContext, 5, 0, RoundedCornersTransformation.CornerType.OTHER_BOTTOM_LEFT))).into(imageView);
                 break;
             case COURSE_TYPE_TWO:
                 // do something
--- app/src/main/java/com/hzcwtech/imooc/fragment/HomeFragment.java
+++ app/src/main/java/com/hzcwtech/imooc/fragment/HomeFragment.java
@@ -108,7 +108,24 @@ public class HomeFragment extends BaseFragment {
         }
         tabRecyclerView.setAdapter(new HomeTabRecAdapter(homeTabData));
 
-        courseRecomRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        courseRecomRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2) {
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        });
+        courseRecomRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                //不是第一个的格子都设一个左边和底部的间距
+                outRect.left = 22;
+                outRect.bottom = 22;
+                //由于每行都只有3个，所以第一个都是3的倍数，把左边距设为0
+                if (parent.getChildLayoutPosition(view) % 2 == 0) {
+                    outRect.left = 0;
+                }
+            }
+        });
         homeCourseData = new ArrayList<>();
         String jsonData = "{\n" +
                 "          \"bgcolor_end\": \"#b3ff739b\",\n" +

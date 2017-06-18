059bbc9ff5afd8f511149f6d82cf16bd06746d43
talon <lily@hzcwtech.com>
2017-06-18 22:07:50 +0800

    。。。

---
+++ app/src/main/java/com/hzcwtech/imooc/entity/model/UserDetailModel.java
@@ -0,0 +1,305 @@
+package com.hzcwtech.imooc.entity.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Talon on 2017/6/18.
+ */
+
+public class UserDetailModel implements Serializable {
+    private static final long serialVersionUID = -7104705595719077056L;
+
+    /**
+     * about_me : 在这偌大的城市里，梦想变得遥不可及。
+     * admin_type : 0
+     * admin_url :
+     * author : 0
+     * bbsnotice : 0
+     * city :
+     * company_id : 0
+     * coursetypesum : 0
+     * courtype : 0
+     * credit : 0
+     * email : 731838568@qq.com
+     * fans : 2
+     * follows : 5
+     * integral : 1
+     * is_v : 0
+     * job : 学生
+     * learn : 45小时26分
+     * mp : 1987
+     * nickname : T_alon
+     * notice : 0
+     * phone :
+     * pic : http://img.mukewang.com/55dc87c70001d7f001000100-370-370.jpg
+     * realname :
+     * school :
+     * sex : 1
+     * shield : 0
+     * tag : 这位童鞋很懒，什么也没有留下～～！
+     * uid : 2288824
+     * uuid : 28915fb80011305ade18ef7757052d64
+     */
+
+    private String about_me;
+    private int admin_type;
+    private String admin_url;
+    private int author;
+    private int bbsnotice;
+    private String city;
+    private int company_id;
+    private int coursetypesum;
+    private int courtype;
+    private int credit;
+    private String email;
+    private int fans;
+    private int follows;
+    private int integral;
+    private int is_v;
+    private String job;
+    private String learn;
+    private int mp;
+    private String nickname;
+    private int notice;
+    private String phone;
+    private String pic;
+    private String realname;
+    private String school;
+    private int sex;
+    private int shield;
+    private String tag;
+    private int uid;
+    private String uuid;
+
+    public String getAbout_me() {
+        return about_me;
+    }
+
+    public void setAbout_me(String about_me) {
+        this.about_me = about_me;
+    }
+
+    public int getAdmin_type() {
+        return admin_type;
+    }
+
+    public void setAdmin_type(int admin_type) {
+        this.admin_type = admin_type;
+    }
+
+    public String getAdmin_url() {
+        return admin_url;
+    }
+
+    public void setAdmin_url(String admin_url) {
+        this.admin_url = admin_url;
+    }
+
+    public int getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(int author) {
+        this.author = author;
+    }
+
+    public int getBbsnotice() {
+        return bbsnotice;
+    }
+
+    public void setBbsnotice(int bbsnotice) {
+        this.bbsnotice = bbsnotice;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getCompany_id() {
+        return company_id;
+    }
+
+    public void setCompany_id(int company_id) {
+        this.company_id = company_id;
+    }
+
+    public int getCoursetypesum() {
+        return coursetypesum;
+    }
+
+    public void setCoursetypesum(int coursetypesum) {
+        this.coursetypesum = coursetypesum;
+    }
+
+    public int getCourtype() {
+        return courtype;
+    }
+
+    public void setCourtype(int courtype) {
+        this.courtype = courtype;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getFans() {
+        return fans;
+    }
+
+    public void setFans(int fans) {
+        this.fans = fans;
+    }
+
+    public int getFollows() {
+        return follows;
+    }
+
+    public void setFollows(int follows) {
+        this.follows = follows;
+    }
+
+    public int getIntegral() {
+        return integral;
+    }
+
+    public void setIntegral(int integral) {
+        this.integral = integral;
+    }
+
+    public int getIs_v() {
+        return is_v;
+    }
+
+    public void setIs_v(int is_v) {
+        this.is_v = is_v;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getLearn() {
+        return learn;
+    }
+
+    public void setLearn(String learn) {
+        this.learn = learn;
+    }
+
+    public int getMp() {
+        return mp;
+    }
+
+    public void setMp(int mp) {
+        this.mp = mp;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getNotice() {
+        return notice;
+    }
+
+    public void setNotice(int notice) {
+        this.notice = notice;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getShield() {
+        return shield;
+    }
+
+    public void setShield(int shield) {
+        this.shield = shield;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
--- app/src/main/java/com/hzcwtech/imooc/ui/main/MineFragment.java
+++ app/src/main/java/com/hzcwtech/imooc/ui/main/MineFragment.java
@@ -12,8 +12,20 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.hzcwtech.imooc.R;
 import com.hzcwtech.imooc.base.BaseFragment;
+import com.hzcwtech.imooc.entity.HttpEntity;
+import com.hzcwtech.imooc.entity.model.AllCourseModel;
+import com.hzcwtech.imooc.entity.model.UserDetailModel;
+import com.hzcwtech.imooc.utils.CommonUtil;
+import com.hzcwtech.imooc.utils.GlideCircleTransform;
+import com.kyleduo.switchbutton.SwitchButton;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,6 +47,20 @@ public class MineFragment extends BaseFragment {
     @BindView(R.id.toolbar_shopping_cart)
     ImageView toolbarShoppingCart;
     Unbinder unbinder;
+    @BindView(R.id.img_user_pic)
+    ImageView imgUserPic;
+    @BindView(R.id.tv_study_time)
+    TextView tvStudyTime;
+    @BindView(R.id.tv_user_nickname)
+    TextView tvUserNickname;
+    @BindView(R.id.tv_user_follows)
+    TextView tvUserFollows;
+    @BindView(R.id.tv_user_fans)
+    TextView tvUserFans;
+    @BindView(R.id.tv_user_integral)
+    TextView tvUserIntegral;
+    @BindView(R.id.receive_new_msg)
+    SwitchButton receiveNewMsg;
 
     public static MineFragment newInstance() {
 
@@ -56,7 +82,7 @@ public class MineFragment extends BaseFragment {
         Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
     }
-    
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -112,6 +138,61 @@ public class MineFragment extends BaseFragment {
 
     private void initView() {
         toolbarName.setText("我的");
+        String jsonData = "{\n" +
+                "  \"data\": [\n" +
+                "    {\n" +
+                "      \"about_me\": \"在这偌大的城市里，梦想变得遥不可及。\",\n" +
+                "      \"admin_type\": 0,\n" +
+                "      \"admin_url\": \"\",\n" +
+                "      \"author\": 0,\n" +
+                "      \"bbsnotice\": 0,\n" +
+                "      \"city\": \"\",\n" +
+                "      \"company_id\": 0,\n" +
+                "      \"coursetypesum\": 0,\n" +
+                "      \"courtype\": 0,\n" +
+                "      \"credit\": 0,\n" +
+                "      \"email\": \"731838568@qq.com\",\n" +
+                "      \"fans\": 2,\n" +
+                "      \"follows\": 5,\n" +
+                "      \"integral\": 1,\n" +
+                "      \"is_v\": 0,\n" +
+                "      \"job\": \"学生\",\n" +
+                "      \"learn\": \"45小时26分\",\n" +
+                "      \"mp\": 1987,\n" +
+                "      \"nickname\": \"T_alon\",\n" +
+                "      \"notice\": 0,\n" +
+                "      \"phone\": \"\",\n" +
+                "      \"pic\": \"http://img.mukewang.com/55dc87c70001d7f001000100-370-370.jpg\",\n" +
+                "      \"realname\": \"\",\n" +
+                "      \"school\": \"\",\n" +
+                "      \"sex\": 1,\n" +
+                "      \"shield\": 0,\n" +
+                "      \"tag\": \"这位童鞋很懒，什么也没有留下～～！\",\n" +
+                "      \"uid\": 2288824,\n" +
+                "      \"uuid\": \"28915fb80011305ade18ef7757052d64\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"errorCode\": 1000,\n" +
+                "  \"errorDesc\": \"成功\",\n" +
+                "  \"status\": 1,\n" +
+                "  \"timestamp\": 1497247861806\n" +
+                "}";
+        HttpEntity httpEntity = JSON.parseObject(jsonData, HttpEntity.class);
+        if (httpEntity.isSuccess()) {
+            List<UserDetailModel> userDetailModelList = httpEntity.getDataObject(new TypeReference<List<UserDetailModel>>() {
+            });
+            UserDetailModel userDetail = userDetailModelList.get(0);
+            if (userDetail != null) {
+                String learnTime = userDetail.getLearn().substring(0, userDetail.getLearn().indexOf('时') + 1);
+                tvUserNickname.setText(userDetail.getNickname());
+                tvStudyTime.setText(CommonUtil.fillString(getContext(), R.string.format_learn_time, learnTime));
+                tvUserFollows.setText(CommonUtil.fillString(getContext(), R.string.format_follows, userDetail.getFollows()));
+                tvUserFans.setText(CommonUtil.fillString(getContext(), R.string.format_fans, userDetail.getFans()));
+                tvUserIntegral.setText(CommonUtil.fillString(getContext(), R.string.format_integral, userDetail.getIntegral()));
+                Glide.with(getContext()).load(userDetail.getPic()).apply(new RequestOptions().transform(new GlideCircleTransform(getContext()))).into(imgUserPic);
+            }
+        }
+
     }
 
     @Override
